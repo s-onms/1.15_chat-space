ce67a1f1f3b9b7d25facf125c83595d7990c672c
s-onms <sok5lunaamspectgdmatu@outlook.jp>
2020-02-10 22:21:51 +0900

    2.10 インデント修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -11,8 +11,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   # storage :file
   storage :fog
 
-  
-
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
